9cf8637fa37ac07b3d9e181b8f9972d2e1fc9efd
Govind Dangi <govinddangi580@gmail.com>
2026-03-02 23:49:24 +0530

    String sheet complete

---
+++ PracticeSheet/src/String/CheckString.java
@@ -0,0 +1,18 @@
+package String;
+
+public class CheckString {
+	public static void main(String[] args) {
+		String str = "12345";
+
+		boolean isDigit = true;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				isDigit = false;
+				break;
+			}
+		}
+
+		System.out.println(isDigit);
+	}
+}
---
+++ PracticeSheet/src/String/FirstNonRepeatingChar.java
@@ -0,0 +1,23 @@
+package String;
+
+public class FirstNonRepeatingChar {
+	public static void main(String[] args) {
+		String str = "Java";
+
+		boolean result = true;
+		for (int i = 0; i < str.length(); i++) {
+			int count = 0;
+			for (int j = 0; j < str.length(); j++) {
+
+				if (str.charAt(i) == str.charAt(j)) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				System.out.println(str.charAt(i));
+				break;
+			}
+		}
+
+	}
+}
